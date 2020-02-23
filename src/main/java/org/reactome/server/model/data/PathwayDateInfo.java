9d2be8a35be256c2444c3c7926edd45313a90cf9
cgong <cgong@ebi.ac.uk>
2020-02-23 18:25:54 +0000

    add method to calculate age of pathway

--- src/main/java/org/reactome/server/model/data/PathwayDateInfo.java
+++ src/main/java/org/reactome/server/model/data/PathwayDateInfo.java
@@ -1,10 +1,17 @@
 package org.reactome.server.model.data;
 
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+
 public class PathwayDateInfo {
     private String stId;
     private String lastAuthored;
     private String lastReviewed;
-    private int age;
+
+    private Integer age;
 
     public String getStId() {
         return stId;
@@ -30,11 +37,34 @@ public class PathwayDateInfo {
         this.lastReviewed = lastReviewed;
     }
 
-    public int getAge() {
+    public Integer getAge(String lastAuthored, String lastReviewed) {
+
+        LocalDate finalDate;
+
+        //2006-10-10 13:07:07.0 ; reviewedDate is 2007-11-08 20:39:37.0
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S");
+
+        if (lastReviewed != null && !lastReviewed.isEmpty()) {
+            LocalDate authored = LocalDate.parse(lastAuthored, formatter);
+            LocalDate reviewed = LocalDate.parse(lastReviewed, formatter);
+
+            if (reviewed.isAfter(authored)) {
+                finalDate = reviewed;
+            } else {
+                finalDate = authored;
+            }
+        } else {
+            finalDate = LocalDate.parse(lastAuthored, formatter);
+        }
+
+        LocalDate start = LocalDate.of(finalDate.getYear(), finalDate.getMonth(), finalDate.getDayOfMonth());
+        LocalDate end = LocalDate.now();
+        age = Math.toIntExact(ChronoUnit.YEARS.between(start, end));
+
         return age;
     }
 
-    public void setAge(int age) {
+    public void setAge(Integer age) {
         this.age = age;
     }
 }
